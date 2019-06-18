cc7e805bdaafa2083c4358b08e61694a0af5476f
samantha <sjatkins@gmail>
2019-06-17 21:13:11 -0600

    WIP

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='cwutils',
       license='internal',
       packages=['cwutils'],
       scripts=['cwutils/word_pass'],
-      install_requires = ['validators'],
+      install_requires = ['validators', 'requests', 'beautifulsoup4'],
       zip_safe=False)
