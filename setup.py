612920abca0f55774ca9d5699cb5a4477c52201f
Samantha Atkins <sjatkins@gmail.com>
2022-10-11 01:23:36 -0600

    fix setup

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 __author__ = 'samantha'
 from setuptools import setup, find_packages
+packages = find_packages(exclude=['test'])
 
 setup(name='sjautils',
       version='0.1',
@@ -7,7 +8,7 @@ setup(name='sjautils',
       author='Samantha Atkins',
       author_email='samantha@conceptwareinc.com',
       license='internal',
-      packages=find_packages('sjautils', exclude='test'),
+      packages=packages,
       scripts=['sjautils/word_pass'],
       install_requires = ['validators', 'requests', 'beautifulsoup4', 'pyyaml'],
       zip_safe=False)
