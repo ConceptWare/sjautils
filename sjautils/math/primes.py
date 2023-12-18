0715d275ad8b2d5a4ffcccaac8dc8594a81df1f0
Samantha Atkins <samantha@conceptwareinc.com>
2023-12-17 20:39:34 -0700

    fixed factoring

--- sjautils/math/primes.py
+++ sjautils/math/primes.py
@@ -1,9 +1,18 @@
 from math import sqrt
 from sjautils.iterext import while_satisfying, while_not_satisfying
 from sjautils import iterext as itx
+from collections import defaultdict
+from math import prod
 
 class Primes:
     known_primes = [2, 3, 5, 7, 11, 13]
+    prime_set = set(known_primes)
+
+    @classmethod
+    def add_prime(cls, p, succ=False):
+        if succ:
+            cls.known_primes.append(p)
+        cls.prime_set.add(p)
 
     @classmethod
     def satisfying(cls, test):
@@ -31,16 +40,65 @@ class Primes:
 
     @classmethod
     def is_prime(cls, n, filtered=0):
-        return cls.filter_moduli(n, lambda x: x == filtered)
+        if n in cls.prime_set:
+            return True
+        mod6 = n % 6
+        if mod6 == 1 or mod6 == 5:
+                
+            return cls.filter_moduli(n, lambda x: x == filtered)
+        else:
+            return False
 
+    
     @classmethod
     def while_le(cls, val):
         return cls.satisfying(lambda n: n <= val)
 
     @classmethod
     def moduli(cls, n):
-        for p in cls.while_le(sqrt(n)):
-            yield n % p
+        return (n%p for p in cls.while_le(sqrt(n)))
+
+    @classmethod
+    def factor(cls, n):
+        # TODO fix me to return defaultdict(int) and use better
+        # algorithm
+        
+
+        factors = defaultdict(int)
+        working = [n]
+
+        low_factors = list(cls.while_le(sqrt(n)))
+    
+        #print(n, factors, low_factors)
+
+        def compute_exp(p):
+            while not working[0] % p:
+                factors[p] += 1
+                working[0] //= p
+            #print('factors added', p, factors[p])
+            
+            
+        for p in low_factors:
+            #print('checking', p, n, working)
+            if working[0] == 1:
+                break
+            if not working[0] % p:
+                compute_exp(p)
+
+        if working[0] != 1:
+            factors[working[0]] = 1
+
+        return factors
+            
+                
+
+                
+                
+
+    
+        
+        
+
 
     @classmethod
     def is_pair_max(cls, n, sep=2):
@@ -58,6 +116,24 @@ class Primes:
         while True:
             if self.is_prime(self._test):
                 self._last = self._test
-                self.known_primes.append(self._last)
+                self.add_prime(self._last, succ=True)
                 yield self._last
             self._test += 2
+
+def factor_string(factors):
+    def prime_term(base, exponent):
+        if exponent == 1:
+            return f'{base}'
+        else:
+            return f'{base}^{exponent}'
+        
+    terms = [prime_term(k,v) for k,v in factors.items()]
+    return ' * '.join(terms)
+    
+def test_factors(n):
+    factors = Primes.factor(n)
+    vals = [k**v for k,v in factors.items()]
+    the_prod = prod(vals)
+    assert the_prod == n, f'{factor_string(factors)} should equal {the_sum}'
+
+    
